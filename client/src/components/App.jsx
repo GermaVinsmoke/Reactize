4a63cbc2d90cfb7c6e8a489299c59240ad348989
GermaVinsmoke <vaibhav1180@gmail.com>
2019-05-13 21:31:26 +0530

    Test

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 class App extends Component {
 
     connectToServer = () => {
+        console.log("Fetching?")
         fetch('/');
     }
 
